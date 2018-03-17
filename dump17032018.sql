36f2ca96b7b199a5e21cfcd7b9ecf5f600eef07c
Bruno Lazaro dos Santos <brunus.ls@gmail.com>
2018-03-17 13:26:28 -0400

    Renomeando campo denunciado para parte

---
+++ dump17032018.sql
@@ -0,0 +1,206 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cpp
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `area_atuacao`
+--
+
+DROP TABLE IF EXISTS `area_atuacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `area_atuacao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_vara` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `area_atuacao`
+--
+
+LOCK TABLES `area_atuacao` WRITE;
+/*!40000 ALTER TABLE `area_atuacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `area_atuacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manifestacao`
+--
+
+DROP TABLE IF EXISTS `manifestacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manifestacao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_procedimento` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manifestacao`
+--
+
+LOCK TABLES `manifestacao` WRITE;
+/*!40000 ALTER TABLE `manifestacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manifestacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movimento`
+--
+
+DROP TABLE IF EXISTS `movimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movimento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_processo` int(11) NOT NULL,
+  `manifestacao` varchar(255) NOT NULL,
+  `destino` varchar(50) NOT NULL,
+  `responsavel` int(11) DEFAULT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movimento`
+--
+
+LOCK TABLES `movimento` WRITE;
+/*!40000 ALTER TABLE `movimento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movimento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `procedimento`
+--
+
+DROP TABLE IF EXISTS `procedimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `procedimento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_area_atuacao` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `procedimento`
+--
+
+LOCK TABLES `procedimento` WRITE;
+/*!40000 ALTER TABLE `procedimento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `procedimento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `processo`
+--
+
+DROP TABLE IF EXISTS `processo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `processo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comarca` varchar(50) DEFAULT NULL,
+  `num_proc` varchar(20) NOT NULL,
+  `id_vara` int(11) NOT NULL,
+  `classe` varchar(255) DEFAULT NULL,
+  `assunto` varchar(255) DEFAULT NULL,
+  `distribuicao` varchar(255) DEFAULT NULL,
+  `autor` varchar(100) DEFAULT NULL,
+  `parte` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `processo`
+--
+
+LOCK TABLES `processo` WRITE;
+/*!40000 ALTER TABLE `processo` DISABLE KEYS */;
+INSERT INTO `processo` VALUES (1,'Pimenta Bueno','10011235520188220009',1,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `processo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vara`
+--
+
+DROP TABLE IF EXISTS `vara`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vara` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vara`
+--
+
+LOCK TABLES `vara` WRITE;
+/*!40000 ALTER TABLE `vara` DISABLE KEYS */;
+INSERT INTO `vara` VALUES (1,'1ª Vara Criminal');
+/*!40000 ALTER TABLE `vara` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-17 13:25:28
